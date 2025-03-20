fa3e6dd120e56477eb3acf0ba86c2f2fda28a72d
Cece111 <cv.04@laposte.net>
2025-03-20 15:00:54 +0100

    api ping ok

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { initHandlers } from "./handlers/handlers";
 
 const app=()=> {
     const app = express();
     const port = 3200;
     app.use(express.json());
+    initHandlers(app);
 
     app.listen(port, ()=>{
         console.log(`Server is running at http://localhost:${port}`);
